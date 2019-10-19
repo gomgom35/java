f67b6a3cdd0a35ed34f4ec61efe07c2af31c27ea
gomgom35 <55079158+gomgom35@users.noreply.github.com>
2019-10-20 04:47:19 +0900

    변수 Varialble

---
+++ src/Variable.java
@@ -0,0 +1,35 @@
+
+public class Variable {
+	/*final 바뀔 수 없는값, 메인함수 밖에 선언 (상수) `1`
+	final static double PI = 3.141592;*/
+	
+	/*final static int INT_MAX = 2147483647; `2`*/
+	
+	public static void main(String[] args)
+	
+											{
+		int a = 1;
+		int b = 2;
+		System.out.println("a + b= " + (a + b));
+		System.out.println("a - b= " + (a - b));
+		System.out.println("a * b= " + (a * b));
+		System.out.println("a / b= " + (a / b));
+		
+		/*int r = 30; `1`
+		System.out.println(r * r * PI);*/
+		
+		/*int a = INT_MAX; `2`
+		System.out.println(a + 1);
+		
+		
+		/*정수.상수,문자열
+		int intType = 100;
+		double doubleType = 150.5;
+		String stringType = "Jenny";
+		
+		System.out.println(intType);
+		System.out.println(doubleType);
+		System.out.println(stringType);*/
+	}
+
+}
