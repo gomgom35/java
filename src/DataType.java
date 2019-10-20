29b4bb4da3e4ed1a4e95bc8d6c1f191e7936fc34
gomgom35 <55079158+gomgom35@users.noreply.github.com>
2019-10-20 15:03:11 +0900

    Data Type

---
+++ src/DataType.java
@@ -0,0 +1,32 @@
+
+public class DataType {
+
+	public static void main(String[] args) {
+		/* double 형
+		double a = 10.3;
+		double b = 9.6;
+		double c = 10.1;
+		
+		System.out.println((a + b + c)/3);*/
+		
+		/*아스키코드..? 반복문
+		for(char i = 'a'; i <= 'z'; i++)
+		{
+			System.out.print(i + " ");	
+		}*/
+		
+		/*10진수를 8,16진수로
+		int a = 200;
+		System.out.println("10진수= "+ a);
+		System.out.format("8진수= %o\n", a);
+		System.out.format("10진수= %x", a);*/
+		
+		/*String 의 Substring 함수 활용
+		String name = "John Smith";
+		System.out.println(name);
+		System.out.println(name.substring(0,1));
+		System.out.println(name.substring(3,6));*/
+		
+	}
+
+}
