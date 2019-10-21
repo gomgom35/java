933b3121cf20174f665a35825af308c82575520d
gomgom35 <55079158+gomgom35@users.noreply.github.com>
2019-10-21 15:22:31 +0900

    기초 테스트

---
+++ src/test1.java
@@ -0,0 +1,27 @@
+
+public class test1 {
+
+	public static void main(String[] args) {
+
+		//System.out.println("Jenny");
+		
+		/*System.out.println(10 + 10); // 20
+		System.out.println(30 * 30); // 900
+		System.out.println(20 - 5); // 15
+		System.out.println(40 / 3); // 13
+		System.out.println(395 % 18); // 17*/
+		
+		
+		for(int i = 0; i < 10; i++)
+		{
+			
+			for(int j = 0; j < 10; j++) 
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}		
+		
+	}
+
+}
