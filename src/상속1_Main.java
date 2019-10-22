939f09e639f0fd841a6bd1816d47626b9bcd3218
gomgom35 <55079158+gomgom35@users.noreply.github.com>
2019-10-22 19:50:49 +0900

    상속

---
+++ src/상속1_Main.java
@@ -0,0 +1,10 @@
+
+public class 상속1_Main {
+
+	public static void main(String[] args) {
+		
+		상속1_Student student = new 상속1_Student("홍길동", 28, 170, 80, "1910120123", 1, 4.5);
+		student.show();
+	}
+
+}
